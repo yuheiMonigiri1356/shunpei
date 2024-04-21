43817ac9e59d13378e278c1e755984e2b7ce4054
yuheiMonigiri1356 <songcunyouping@gmail.com>
2024-04-22 08:19:12 +0900

    dev_basisの追加

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index(Post $post)
+    {
+        return view('posts.index')->with(['posts' => $post->getPaginateByLimit(1)]);
+    }
+    
+    public function show(Post $post)
+    {
+        return view('posts/show')->with(['post' => $post]);
+    }
+}
