fedc8e73df8c933c1a9244478eaad784fcb44e2c
yuheiMonigiri1356 <songcunyouping@gmail.com>
2024-05-12 08:53:44 +0900

    8-6追加

---
+++ resources/views/posts/edit.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <title>Blog</title>
+    </head>
+    <body>
+        <h1 class="title">編集画面</h1>
+        <div class="content">
+            <form action="/posts/{{ $post->id }}" method="POST">
+                @csrf
+                @method('PUT')
+                <div class='content_title'>
+                    <h2>タイトル</h2>
+                    <input type="text" name='post[title]' value="{{ $post->title }}"/>
+                </div>
+                <div class='content_body'>
+                    <h2>本文</h2>
+                    <input type='text' name='post[body]' value="{{ $post->body }}">
+                </div>
+                <input type="submit" value="update"/>
+                
+            </form>
+            <div class='footer'>
+                <a href="/posts/{{ $post->id }}">戻る</a>
+            </div>
+        </div>
+    </body>
+</html>
